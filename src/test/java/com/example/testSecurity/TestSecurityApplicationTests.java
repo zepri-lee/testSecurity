6b390c8aa2c7a80573e2b95b3ed6ed6bea67a271
zepri-lee <zepri123@gmail.com>
2024-04-19 19:06:01 +0900

    initial

---
+++ src/test/java/com/example/testSecurity/TestSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
